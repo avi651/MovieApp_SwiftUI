4dedd2f58a51d8a71e67e49eae8753a5cbd1c590
Avinash Kumar <kumaravi651@gmail.com>
2024-02-26 15:23:53 +0530

    Initial Commit

---
+++ MovieApp/MovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppApp.swift
+//  MovieApp
+//
+//  Created by Avinash on 26/02/24.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
